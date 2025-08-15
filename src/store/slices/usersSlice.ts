5940cae0a2d8eb09b3de9f946422e249ddc56ee6
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-15 13:22:06 +0100

    add users related types

--- src/store/slices/usersSlice.ts
+++ src/store/slices/usersSlice.ts
@@ -1,10 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { User } from "../../types/user";
+
+interface UserSliceState {
+  data: User[];
+  isLoading: boolean;
+  error: string | null;
+}
+
+const initialState: UserSliceState = {
+  data: [],
+  isLoading: false,
+  error: null,
+};
 
 const userSlice = createSlice({
   name: "users",
-  initialState: {
-    data: [],
-  },
+  initialState,
   reducers: {},
 });
 

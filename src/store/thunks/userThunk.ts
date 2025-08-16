502939cfbb891ca4aa23a5c3f113ba46b2fd3145
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-16 12:44:06 +0100

    simulate data fecthing

--- src/store/thunks/userThunk.ts
+++ src/store/thunks/userThunk.ts
@@ -4,8 +4,15 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const resp = await axios.get("http://localhost:3005/users");
   const data = await resp.data;
-  console.log(data);
+  console.log(import.meta.env.VITE_SIMULATE_DATA_FETCHING);
+
+  if (import.meta.env.VITE_SIMULATE_DATA_FETCHING) {
+    await pause(1000);
+  }
   return data;
 });
+
+const pause = (durration: number) =>
+  new Promise((resolve) => setTimeout(resolve, durration));
 
 export { fetchUsers };

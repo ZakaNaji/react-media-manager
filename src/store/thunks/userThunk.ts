d7689dddf9f2df221928b8ccdc79fd32ca2c5956
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-17 13:37:30 +0100

    delete user thunk function

--- src/store/thunks/userThunk.ts
+++ src/store/thunks/userThunk.ts
@@ -1,29 +1,44 @@
 import { faker } from "@faker-js/faker";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import type { User } from "../../types/user";
 
 const apiRootURL = import.meta.env.VITE_API_ROOT;
 
-const fetchUsers = createAsyncThunk("users/fetch", async () => {
-  const resp = await axios.get(`${apiRootURL}/users`);
-  const data = await resp.data;
+const fetchUsers = createAsyncThunk<User, void, { rejectValue: string }>(
+  "users/fetch",
+  async () => {
+    const resp = await axios.get(`${apiRootURL}/users`);
+    const data = await resp.data;
 
-  await pause(1000);
+    await pause(1000);
 
-  return data;
-});
+    return data;
+  }
+);
 
-const addUser = createAsyncThunk("users/add", async () => {
-  const newUser = { name: faker.name.firstName() };
-  const resp = await axios.post(`${apiRootURL}/users`, newUser);
-  const data = await resp.data;
+const addUser = createAsyncThunk<User, void, { rejectValue: string }>(
+  "users/add",
+  async () => {
+    const newUser = { name: faker.name.firstName() };
+    const resp = await axios.post(`${apiRootURL}/users`, newUser);
+    const data = await resp.data;
 
-  await pause(1000);
-  return data;
-});
+    await pause(1000);
+    return data;
+  }
+);
 
-const deleteUser = createAsyncThunk("users/delete", async (id: string) => {
-  const resp = await axios.delete(`${apiRootURL}/users/${id}`);
+const deleteUser = createAsyncThunk<
+  string, // Returned
+  string,
+  { rejectValue: string } // 👈 string error
+>("users/delete", async (id: string, { rejectWithValue }) => {
+  try {
+    const resp = await axios.delete(`${apiRootURL}/users/${id}`);
+  } catch (e) {
+    return rejectWithValue("Error deleting user: ");
+  }
 
   return id;
 });
@@ -35,5 +50,3 @@ const pause = (durration: number) => {
 };
 
 export { fetchUsers, addUser, deleteUser };
-
-export type thunkType = typeof fetchUsers | typeof deleteUser;

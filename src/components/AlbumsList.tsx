7312d6db8e4dcec90c63b59f203c7577ebb70b43
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-18 15:52:17 +0100

    use the auto-genrated hook inside our albumsList component

--- src/components/AlbumsList.tsx
+++ src/components/AlbumsList.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import type { User } from "../types/user";
+import { useGetAlbumsByUserIdQuery } from "../hooks/albumsApi";
 
 export default function AlbumsList({ user }: { user: User }) {
-  return <div>AlbumsList for user: {user.name}</div>;
+  const {
+    data: albums,
+    isLoading,
+    isError,
+  } = useGetAlbumsByUserIdQuery(user.id);
+  return (
+    <div>
+      {albums?.map((album) => (
+        <div key={album.id}>{album.title}</div>
+      ))}
+    </div>
+  );
 }

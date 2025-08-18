80ada33c6a7145bb6460ebd5ba4a62db500f59a9
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-18 15:51:53 +0100

    add the new created api slice to our store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "../hooks/albumsApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
   },
+  middleware: (defaultMiddleware) =>
+    defaultMiddleware().concat(albumsApi.middleware),
 });
 
 export type RootType = ReturnType<typeof store.getState>;

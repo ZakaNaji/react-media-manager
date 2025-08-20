068a09f4f9c023427d9f89fd66f97bf6d2e456b4
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-08-20 15:18:17 +0100

    add delete logic to AlbumsListItem

--- src/components/AlbumsListitem.tsx
+++ src/components/AlbumsListitem.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import type { Album } from "../types/album";
 import Expandablepanel from "./Expandablepanel";
 import Header from "./Header";
+import { useDeleteAlbumMutation } from "../hooks/albumsApi";
 
 export default function AlbumsListitem({ album }: { album: Album }) {
+  const [deleteAlbum, deleteResults] = useDeleteAlbumMutation();
+
+  const handleClick = (e: React.MouseEvent, album: Album) => {
+    e.stopPropagation();
+    deleteAlbum(album);
+  };
   return (
     <Expandablepanel
       header={
-        <Header data={album.title} isLoading={false} handleClick={() => {}} />
+        <Header
+          data={album.title}
+          isLoading={deleteResults.isLoading}
+          handleClick={(e) => handleClick(e, album)}
+        />
       }
     >
       Test
